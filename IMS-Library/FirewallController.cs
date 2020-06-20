4a0fd70194a31447a7f2d286ab990cfdbbad49d0
Douglas M. Dwyer <dougmdwyer@hotmail.com>
2020-06-20 17:34:54 -0400

    Debug release 0.1.0.4 to determine issues with the Windows firewall

--- IMS-Library/FirewallController.cs
+++ IMS-Library/FirewallController.cs
@@ -20,6 +20,7 @@ namespace IMS_Library
         public void CreateFirewallExecutableException(string name, string path)
         {
             string error;
+            Logger.WriteInfo("Trying to forward " + "netsh advfirewall firewall add rule name=\"IMS E" + name + "\" dir=in protocol=tcp program=\"" + path.Replace("/", "\\") + "\" profile=any action=allow");
             if(Extensions.ExecuteShellCommand("netsh advfirewall firewall add rule name=\"IMS E" + name + "\" dir=in protocol=tcp program=\"" + path.Replace("/", "\\") + "\" profile=any action=allow", out error) != 0)
             {
                 IMS.Instance.UserMessageManager.LogWarning("IMS was unable to make an exception in the windows firewall.", false);
